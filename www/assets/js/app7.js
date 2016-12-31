269f3d19b4abadc6ac2ded27ac8faf4a7433cb0a
vezixtor <everson___@hotmail.com>
2016-12-31 03:52:43 -0200

    Calendario

--- www/assets/js/app7.js
+++ www/assets/js/app7.js
@@ -10,48 +10,65 @@ var mainView = myApp.addView('.view-main', {
   dynamicNavbar: true
 });
 
-$$('#tab-1').on('tab:show', function () {
-    myApp.alert('Tab 1 is visible');
-});
+//API
+var apiUrl = 'http://www.kashew.tecnologia.ws/agendamacom/';
 
+
+//dashboard.html
 var monthNames = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto' , 'Setembro' , 'Outubro', 'Novembro', 'Dezembro'];
 var semanaNomes = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 var eventos =  [
   new Date(2016, 11, 25),
   new Date(2017, 0, 1)
 ];
+myApp.onPageInit('dashboard', function (page) {
+  var calendarInline = myApp.calendar({
+        container: '#calendar-inline-container',
+        value: [new Date()],
+        weekHeader: true,
+        dayNamesShort: semanaNomes,
+        events: eventos,
+        toolbarTemplate:
+            '<div class="toolbar calendar-custom-toolbar">' +
+                '<div class="toolbar-inner">' +
+                    '<div class="left">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
+                    '</div>' +
+                    '<div class="center"></div>' +
+                    '<div class="right">' +
+                        '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>',
+        onOpen: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+            $$('.calendar-custom-toolbar .left .link').on('click', function () {
+                calendarInline.prevMonth();
+            });
+            $$('.calendar-custom-toolbar .right .link').on('click', function () {
+                calendarInline.nextMonth();
+            });
+        },
+        onMonthYearChangeStart: function (p) {
+            $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
+        },
+        onDayClick: function (p, dayContainer, year, month, day) {
+          console.log('onDayClick');
+        }
+    });
 
-var calendarInline = myApp.calendar({
-    container: '#calendar-inline-container',
-    value: [new Date()],
-    weekHeader: true,
-    dayNamesShort: semanaNomes,
-    events: eventos,
-    toolbarTemplate:
-        '<div class="toolbar calendar-custom-toolbar">' +
-            '<div class="toolbar-inner">' +
-                '<div class="left">' +
-                    '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
-                '</div>' +
-                '<div class="center"></div>' +
-                '<div class="right">' +
-                    '<a href="#" class="link icon-only"><i class="icon icon-forward"></i></a>' +
-                '</div>' +
-            '</div>' +
-        '</div>',
-    onOpen: function (p) {
-        $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
-        $$('.calendar-custom-toolbar .left .link').on('click', function () {
-            calendarInline.prevMonth();
-        });
-        $$('.calendar-custom-toolbar .right .link').on('click', function () {
-            calendarInline.nextMonth();
-        });
-    },
-    onMonthYearChangeStart: function (p) {
-        $$('.calendar-custom-toolbar .center').text(monthNames[p.currentMonth] +', ' + p.currentYear);
-    },
-    onDayClick: function (p, dayContainer, year, month, day) {
-      console.log('onDayClick');
-    }
+  $$('#tab-1').on('tab:show', function () {
+
+  });
+
+  $$('#tab-2').on('tab:show', function () {
+    console.log('Tab 2 is visible');
+    getLojas();
+  });
+
 });
+function getLojas() {
+  $$.getJSON(apiUrl + 'stores.php', function (data) {
+    console.log(data);
+  });
+}
